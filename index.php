9080ec2cc10c270ad3fcd94f14c770ba47c120d5
Miguel Angel Martinez Ramirez <miguelmr560@hotmail.com>
2026-05-10 19:49:36 +0200

    Se ha actualizado el archivo index.php con una línea nueva

--- index.php
+++ index.php
@@ -7,4 +7,6 @@ echo "<p><strong>Nombre de usuario:</strong> Miguel Ángel Martínez Ramírez</p
 echo "<p><strong>Correo electrónico:</strong> miguelmr560@hotmail.com</p>";
 echo "<p><strong>Versión de Git:</strong> 2.54.0.windows.1</p>";
 
+echo "<p><em>Cambio realizado para demostrar el control de versiones en la Tarea 5.</em></p>";
+
 ?>
