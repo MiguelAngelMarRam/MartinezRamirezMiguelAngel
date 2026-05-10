fc63edc6fd92b38d8ef0dd7787938addf2c83cd8
Miguel Angel Martinez Ramirez <miguelmr560@hotmail.com>
2026-05-10 19:38:26 +0200

    Añadida configuración inicial del ejercicio 1.2

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Configuración de Git realizada en la Actividad 1.2
+// Alumno: Miguel Ángel Martínez Ramírez
+
+echo "<h1>Configuración de mi entorno Git</h1>";
+echo "<p><strong>Nombre de usuario:</strong> Miguel Ángel Martínez Ramírez</p>";
+echo "<p><strong>Correo electrónico:</strong> miguelmr560@hotmail.com</p>";
+echo "<p><strong>Versión de Git:</strong> 2.54.0.windows.1</p>";
+
+?>
